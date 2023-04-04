c8cd66a5fa2dbbb262e1ab48f888750f472c71b5
Mattis Knulst <mattis.knulst@med.lu.se>
2023-04-04 16:21:26 +0200

    add delivery shell script to the project

---
+++ bin/delivery.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+# Usage:
+# bash delivery.sh DATA-TO-DELIVER
+# this script assumes $USER is same on lfs and lsens!
+
+# delivery server root
+lfs_root="/srv/data"
+# first arg is the data to deliver
+data="$1"
+# eg ../Jobs/proj_id
+project_id="$(basename $data)"
+# Set lfs603 ${project_id} and target folders
+lfs_project_dir="${lfs_root}/${project_id}" 
+# generate a password
+password=$(openssl rand --base64 16)
+# remote execution:
+# create user: User_Name:Password:UID:GID:Comments:User_Home_Directory:Users_Shell_Name
+ssh "${USER}@lfs603.srv.lu.se sudo echo '${project_id}:${password}::::${lfs_project_dir}:/bin/bash' | newusers"
+# add user to ssh_users group
+ssh "${USER}@lfs603.srv.lu.se sudo adduser ${project_id} ssh_users"
+# sync directory to lfs603
+rsync -av --progress --human-readable --no-perms "${data%%/} ${$USER}@lfs603.srv.lu.se:${lfs_project_dir}"
+
+# output the password to file
+echo "${password}" > "password.txt"
