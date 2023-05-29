8e1b9f3dea36f22210cf7c9d0b5ec3edfa433bd9
Mattis Knulst <mattis.knulst@med.lu.se>
2023-05-29 13:31:49 +0200

    edit delivery

--- bin/delivery.sh
+++ bin/delivery.sh
@@ -6,9 +6,9 @@
 # delivery server root
 lfs_root="/srv/data"
 # first arg is the data to deliver
-data="$1"
-# eg ../Jobs/proj_id
-project_id="$(basename $data)"
+data=$1
+# eg ../Jobs/proj_id we have to specify this
+project_id=$2
 # Set lfs603 ${project_id} and target folders
 lfs_project_dir="${lfs_root}/${project_id}" 
 # generate a password
@@ -26,7 +26,7 @@ sudo adduser ${project_id} ssh_users
 _remote_cmds
 
 # sync directory to lfs603
-rsync -av --progress --human-readable --no-perms "${data%%/} ${$USER}@lfs603.srv.lu.se:${lfs_project_dir}"
+rsync -rv --rsync-path="sudo rsync" --progress --human-readable --no-perms "${data%%/}" "${$USER}@lfs603.srv.lu.se:${lfs_project_dir}"
 
 # output the password to file
-echo "${password}" > "password.txt"
+echo -e "${project_id}\n${password}\n\n" > "${HOME}/passwords.txt"
