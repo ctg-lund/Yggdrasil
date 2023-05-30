b42a1b4e59df91f0810d0d12d7ee832a5274f982
Mattis Knulst <mattis.knulst@med.lu.se>
2023-05-30 09:32:16 +0200

    added interop_qc

---
+++ bin/interop_qc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+umask 002
+# Usage:
+# bash interop_qc.sh /path/to/runfolder
+runfolder_path=$1
+runfolder_name=$(basename ${runfolder_path})
+interop_id="ctg_interop_${runfolder_name}"
+interop_out="${runfolder_path}/ctg-interop"
+ctg_qc_out="/projects/fs1/shared/ctg-qc/interop"
+interop_plots_script="/projects/fs1/shared/scripts/interop-summaries/bin/interop-plots.sh"
+## singularity container/command for python scripts
+singularity_cmd="singularity exec --bind /projects/ /projects/fs1/shared/ctg-containers/ngs-tools/ngs-tools.sif"
+mkdir -p ${interop_out}
+# Generating interop_summary (Python): "
+${singularity_cmd} interop_summary ${runfolder_path} --csv=1 > ${interop_out}/interop_summary
+# Generating interop_index-summary (Python): "
+${singularity_cmd} interop_index-summary ${runfolder_path} --csv=1 > ${interop_out}/interop_index-summary
+# Generating interop qc plots (./bin/interop-plots.sh): "
+# bash /projects/fs1/shared/ctg-tools/bin/ctg-interop-plots.sh ${runfolder_path}
+bash ${interop_plots_script} ${runfolder_path}
+${singularity_cmd} multiqc -f ${interop_out} -n ${interop_out}/multiqc_${interop_id} 
+
